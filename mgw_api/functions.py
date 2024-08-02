f7e3b2083fedd5d37581df987f4dfb122f521880
desiro <daniel.desiro@web.de>
2024-08-02 19:07:35 +0200

    search update

--- mgw_api/functions.py
+++ mgw_api/functions.py
@@ -1,9 +1,17 @@
 # mgw_api/functions.py
 
 import time  # Simulate a time-consuming task
-from .models import Fasta
+from .models import Fasta, Settings
+from .forms import SettingsForm
+from django.contrib import messages
+from django.shortcuts import redirect
+from django.urls import reverse
+import subprocess
 
 import csv
+import re
+import sys
+import os
 
 def process_pending_files():
     pending_files = Fasta.objects.filter(processed=False)
@@ -32,3 +40,46 @@ def get_numeric_columns(rows):
             except ValueError:
                 if value: string_columns.add(index)
     return not_empty.intersection(all_columns - string_columns)
+
+def apply_regex(rows, column, value):
+    try:
+        regex = re.compile(fr"{value}", re.IGNORECASE)
+        return [row for row in rows if regex.search(row[int(column)])]
+    except:
+        return rows
+
+def is_float(value):
+    try:
+        float(value)
+        return True
+    except (ValueError, TypeError):
+        return False
+    
+def apply_compare(modifier, rows, column, value):
+    try:
+        if modifier * float(rows[int(column)]) >= modifier * float(value): return True
+        else: return False
+    except (ValueError, TypeError):
+        return True
+
+def run_create_signature_and_search(user_id, name, fasta_id):
+    try:
+        manage_py_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'manage.py')
+        fasta = Fasta.objects.get(id=fasta_id)
+        subprocess.run([sys.executable, manage_py_path, "create_signature", str(user_id), name], check=True)
+        result = subprocess.run([sys.executable, manage_py_path, "create_search", str(user_id), name], capture_output=True, text=True, check=True)
+        print(result)
+        result_pks = []
+        for line in result.stdout.split('\n'):
+            if line.startswith("Result PKs:"):
+                result_pks = line.split(':')[1].strip().strip('[]').split(', ')
+        if result_pks:
+            result_pk = int(result_pks[0])
+            fasta.result_pk = result_pk
+            fasta.processed = True
+            fasta.status = "Complete"
+            fasta.save()
+    except subprocess.CalledProcessError as e:
+        print(f"Error during background processing: {e}")
+        fasta.status = f"Error: {e}"
+        fasta.save()
--- mgw_api/management/commands/create_search.py
+++ mgw_api/management/commands/create_search.py
@@ -10,42 +10,68 @@ import subprocess
 import os
 
 
-
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('user_id', type=int, help='ID of the user')
+        parser.add_argument('name', type=str, help='Name of the fasta file')
+    
     def handle(self, *args, **kwargs):
+        user_id = kwargs['user_id']
+        name = kwargs['name']
         now = datetime.now()
         dt = now.strftime("%d.%m.%Y %H:%M:%S")
         log = f"{dt} - create_search - "
-        pending_files = Signature.objects.filter(submitted=True)
-        for signature in pending_files:
-            try:
-                uset = signature.settings_used
-                for k,db in product(uset["kmer"], uset["database"]):
-                    # Search signatures in rocksdb
-                    date = datetime.now().strftime("%Y%m%d")
-                    index_path = os.path.join(settings.EXTERNAL_DATA_DIR, db, "index", f"{db}-{k}.rocksdb")
-                    user_path = os.path.dirname(signature.file.path)
-                    result_file = os.path.join(user_path, f"result_{signature.name}-{db}-{k}-{date}.csv")
-                    self.stdout.write(self.style.SUCCESS(result_file))
-                    result = self.search_index(result_file, signature.file.path, index_path, k, uset["containment"])
-                    if result.returncode != 0:
-                        raise Exception(f"Searching failed with exit code {result.returncode}: {result.stderr}")
-                    # Save result to django model
-                    relative_path = os.path.relpath(result_file, settings.MEDIA_ROOT)
-                    self.stdout.write(self.style.SUCCESS(relative_path))
-                    result_model = Result(user=signature.user, signature=signature, name=signature.name)
-                    result_model.file.name = relative_path
-                    result_model.size = result_model.file.size
-                    result_model.settings_used = uset
-                    result_model.save()
-                    self.stdout.write(self.style.SUCCESS(f"Created at {result_model.created_date}'"))
-                    ## Update the processed status
-                    #self.stdout.write(self.style.SUCCESS(f"Successfully processed file '{fasta.name}'"))
-                    #self.stdout.write(self.style.SUCCESS(f"{log}Searching signature in database successful."))
-            except Exception as e:
-                self.stdout.write(self.style.ERROR(f"{log}Error processing search '{signature.name}': {e}"))
-            signature.submitted = False
-            signature.save()
+        signature = Signature.objects.get(user_id=user_id, name=name, submitted=True)
+        self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} {signature.user_id} {signature.submitted} {signature.file.name}' ####"))
+        try:
+            uset = Settings.objects.get(user=user_id).to_dict()
+            result_pks = []
+            self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 1 ####"))
+            for k,db in product(uset["kmer"], uset["database"]):
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 2 ####"))
+                # Search signatures in rocksdb
+                date = datetime.now().strftime("%Y%m%d")
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 3 ####"))
+                index_path = os.path.join(settings.EXTERNAL_DATA_DIR, db, "index", f"{db}-{k}.rocksdb")
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 4 ####"))
+                user_path = os.path.dirname(signature.file.path)
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 5 ####"))
+                result_file = os.path.join(user_path, f"result_{signature.name}-{db}-{k}-{date}.csv")
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 6 ####"))
+                self.stdout.write(self.style.SUCCESS(result_file))
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 7 ####"))
+                result = self.search_index(result_file, signature.file.path, index_path, k, uset["containment"])
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 8 ####"))
+                if result.returncode != 0:
+                    raise Exception(f"Searching failed with exit code {result.returncode}: {result.stderr}")
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 9 ####"))
+                # Save result to django model
+                relative_path = os.path.relpath(result_file, settings.MEDIA_ROOT)
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 10 ####"))
+                self.stdout.write(self.style.SUCCESS(relative_path))
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 11 ####"))
+                result_model = Result(user=signature.user, signature=signature, name=signature.name)
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 12 ####"))
+                result_model.file.name = relative_path
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 13 ####"))
+                result_model.size = result_model.file.size
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 14 ####"))
+                result_model.settings_used = uset
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 15 ####"))
+                result_model.save()
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 16 ####"))
+                self.stdout.write(self.style.SUCCESS(f"Created at {result_model.created_date}'"))
+                self.stdout.write(self.style.SUCCESS(f"#### TEST {signature.name} 17 ####"))
+                ## Update the processed status
+                #self.stdout.write(self.style.SUCCESS(f"Successfully processed file '{fasta.name}'"))
+                #self.stdout.write(self.style.SUCCESS(f"{log}Searching signature in database successful."))
+                result_pks.append(result_model.pk)
+        except Exception as e:
+            self.stdout.write(self.style.ERROR(f"{log}Error processing search '{signature.name}': {e}"))
+        signature.submitted = False
+        signature.save()
+        if result_pks:
+            self.stdout.write(f"Result PKs: {result_pks}")
 
     def search_index(self, result_file, sketch_file, index_path, k, containment):
         cmd = ["sourmash", "scripts", "manysearch", "--ksize", f"{k}", "--moltype", "DNA", "--scaled", "1000", "--cores", "20", "--threshold", f"{containment}", "--output", result_file, sketch_file, index_path]
@@ -54,82 +80,3 @@ class Command(BaseCommand):
     
     def combine_results(self, k, db):
         pass
-
-
-
-#            sig_list = os.path.join(settings.EXTERNAL_DATA_DIR, database, f"sig-list.txt")
-#            manifest = os.path.join(settings.EXTERNAL_DATA_DIR, database, f"manifest.pcl")
-#            dir_paths = self.handle_dirs(database)
-#            sig_files = self.get_manifest(manifest, dir_paths)
-#            new_files = self.check_updates(sig_files, dir_paths)
-#            if new_files:
-#                self.create_list(new_files, sig_list)
-#                results = [self.update_index(dir_paths, sig_list, database, k) for k in kmers]
-#                td, rs = ("failed", ".err") if sum([int(r.returncode) for r in results]) else ("signatures", "")
-#                self.move_files(new_files, dir_paths, td, rs)
-#                self.update_manifest(new_files, sig_files, manifest)
-#                self.stdout.write(self.style.SUCCESS(f"{log}Updated index and moved new signatures."))
-#
-#
-#
-#
-#
-#
-#
-#                # Save signatures to django model
-#                relative_path = os.path.relpath(signature_file, settings.MEDIA_ROOT)
-#                self.stdout.write(self.style.SUCCESS(relative_path))
-#                signature_model = Signature(user=fasta.user, fasta=fasta, name=fasta.name)
-#                signature_model.file.name = relative_path
-#                signature_model.size = signature_model.file.size
-#                signature_model.save()
-#                ## Update the processed status
-#                self.stdout.write(self.style.SUCCESS(f"Successfully processed file '{fasta.name}'"))
-#                fasta.delete()
-#            except Exception as e:
-#                self.stdout.write(self.style.ERROR(f"Error processing file '{fasta.name}': {e}"))
-#            signature.submitted = False
-#            signature.save()
-#
-#
-#
-#
-#
-#
-#
-#
-#                self.stdout.write(self.style.SUCCESS(f"{log}Updated index and moved new signatures."))
-#            else:
-#                self.stdout.write(self.style.SUCCESS(f"{log}No new files to process in {dir_paths['updates']}."))
-#        except Exception as e:
-#            self.stdout.write(self.style.ERROR(f"{log}Error processing directory '{settings.EXTERNAL_DATA_DIR}': {e}"))
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#    time_s = get_time()
-#    kmers = [21,31,51]
-#    database = "old"
-#    pending_files = glob.glob(os.path.join(EXTERNAL_DATA_DIR, "old", "sketches", "*.sig.gz"))
-#    print(pending_files)
-#    sig_data = list()
-#    for sketch_file in pending_files:
-#        for k in kmers:
-#            sketch_name = os.path.basename(os.path.splitext(os.path.splitext(sketch_file)[0])[0])
-#            search_file = os.path.join(EXTERNAL_DATA_DIR, "old", "search", f"{sketch_name}.csv")
-#            index_path = os.path.join(EXTERNAL_DATA_DIR, "old", "index", f"{database}-{k}.rocksdb")
-#            sig_data.append(search_index(index_path, sketch_file, search_file, k))
-#    print([r.returncode for r in sig_data])
-#    print(sum([int(r.returncode) for r in sig_data]))
-#    exit()
