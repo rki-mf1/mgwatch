01db1e9d23bbb09f1e810a25ef23f039021e4863
Huska, Matthew <HuskaM@rki.de>
2024-10-03 16:09:26 +0200

    First attempt at production docker setup

---
+++ build-docker.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+docker build -t mgwatch:local -f Dockerfile .
--- mgw/settings.py
+++ mgw/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'mgw.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
     }
 }
 
