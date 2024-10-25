5afa1064c542eed567ece63342a38d7040cddc14
Huska, Matthew <HuskaM@rki.de>
2024-10-25 15:58:40 +0200

    Typo in dc-prod.sh script name

--- scripts/prod-manage.sh
+++ scripts/prod-manage.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./scripts/prod-dev.sh run --rm mgwatch-backend "conda run --no-capture-output -n mgw ./manage.py $*"
+./scripts/dc-prod.sh run --rm mgwatch-backend "conda run --no-capture-output -n mgw ./manage.py $*"
