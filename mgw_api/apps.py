b281c7f2cbf47648b53fecde809994bcb74e1fcd
Huska, Matthew <HuskaM@rki.de>
2024-10-25 14:25:17 +0200

    Set up cron in backend container, add separate container to run the cron jobs

--- mgw_api/apps.py
+++ mgw_api/apps.py
@@ -1,6 +1,9 @@
 # mgw_api/apps.py
 
+import os
+import signal
 from django.apps import AppConfig
+from django.core.management import call_command
 
 
 class MgwApiConfig(AppConfig):
@@ -9,3 +12,17 @@ class MgwApiConfig(AppConfig):
 
     def ready(self):
         import mgw_api.signals
+
+        # Extra code to initialize cronjobs. Copied from custom runserver.
+        self.manage_py_path = os.path.abspath("manage.py")
+        print("Starting cron jobs...")
+        call_command('create_crons', 'start', self.manage_py_path)
+
+        # Stop cron jobs on exit
+        signal.signal(signal.SIGINT, self.stop_services)
+        signal.signal(signal.SIGTERM, self.stop_services)
+
+    def stop_services(self, signum, frame):
+        print("Stopping cron jobs...")
+        call_command('create_crons', 'stop', self.manage_py_path)
+        sys.exit(0)
