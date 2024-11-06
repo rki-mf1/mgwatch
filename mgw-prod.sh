302afb38641d96bf1ea734573185bcdd80d4ff45
Huska, Matthew <HuskaM@rki.de>
2024-11-06 15:23:14 +0100

    Add a prod version of the mgw.sh script. Add an option to open logs when everything has started (-l)

--- mgw-prod.sh
+++ mgw-prod.sh
@@ -16,7 +16,7 @@ while getopts "bcmlh" opt; do
     m) MIGRATE=0 ;;
     l) SHOW_LOGS=0 ;;
     h)
-      echo "./mgw.sh [-b] [-c] [-m]"
+      echo "./mgw-prod.sh [-b] [-c] [-m]"
       echo " -b     build backend docker container"
       echo " -c     create (=make) migrations"
       echo " -m     migrate"
@@ -26,9 +26,9 @@ while getopts "bcmlh" opt; do
   esac
 done
 
-./scripts/dc-dev.sh down --remove-orphans
+./scripts/dc-prod.sh down --remove-orphans
 [[ BUILD_CONTAINER -eq 0 ]] && ./scripts/build-docker.sh
-./scripts/dc-dev.sh up -d --force-recreate
-[[ CREATE_MIGRATIONS -eq 0 ]] && ./scripts/dev-manage.sh makemigrations
-[[ MIGRATE -eq 0 ]] && ./scripts/dev-manage.sh migrate
-[[ SHOW_LOGS -eq 0 ]] && ./scripts/dc-dev.sh logs -tf
+./scripts/dc-prod.sh up -d --force-recreate
+[[ CREATE_MIGRATIONS -eq 0 ]] && ./scripts/prod-manage.sh makemigrations
+[[ MIGRATE -eq 0 ]] && ./scripts/prod-manage.sh migrate
+[[ SHOW_LOGS -eq 0 ]] && ./scripts/dc-prod.sh logs -tf
