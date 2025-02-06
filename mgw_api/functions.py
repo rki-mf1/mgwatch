7a99fbbd4fbddfff3caab57b6260e324a88e5246
Huska, Matthew <HuskaM@rki.de>
2025-02-06 14:24:57 +0100

    Initial rewrite to use pandas DataFrames instead of lists

--- mgw_api/functions.py
+++ mgw_api/functions.py
@@ -14,6 +14,53 @@ from mgw.settings import LOGGER
 from .models import Fasta
 
 
+def get_metadata(headers, rows):
+    # 0 = in the SRA mongodb database
+    # 1 = in the search result csv file
+    column_dict = {
+        "sra_link": 0,
+        "assay_type": 0,
+        "bioproject": 0,
+        "biosample_link": 0,
+        "query_containment_ani": 1,
+        "collection_date_sam": 0,
+        "containment": 1,
+        "geo_loc_name_country_calc": 0,
+        "lat_lon": 0,
+        "organism": 0,
+        "containment_threshold": 1,
+        "releasedate": 0,
+        "librarysource": 0,
+    }
+    mongo_df = search_mongodb(headers, rows)
+    csv_df = search_csv(headers, rows, column_dict)
+    combined_df = pd.DataFrame()
+    for column, in_csv in column_dict.items():
+        if in_csv == 1 and column in csv_df.columns:
+            combined_df[column] = csv_df[column]
+        elif in_csv == 0 and column in mongo_df.columns:
+            combined_df[column] = mongo_df[column]
+        else:
+            LOGGER.warning(
+                "Metadata column '%s' missing. Returning an empty string.", column
+            )
+            combined_df[column] = ""
+    new_headers = combined_df.columns.tolist()
+    new_rows = combined_df.values.tolist()
+    return new_headers, new_rows
+
+
+def search_mongodb(headers, rows):
+    mongo = pm.MongoClient(settings.MONGO_URI)
+    db = mongo["sradb"]
+    collection = db["sradb_list"]
+    qidx = headers.index("match_name")
+    query = {"_id": {"$in": [row[qidx] for row in rows]}}
+    mongo_df = pd.DataFrame(list(collection.find(query)))
+    mongo.close()
+    return mongo_df.map(convert_to_string)
+
+
 def get_table_data(result, max_rows=None):
     table_data = []
     with open(result.file.path, newline="") as csvfile:
@@ -41,6 +88,30 @@ def get_numeric_columns(rows):
                 if value:
                     string_columns.add(index)
     return not_empty.intersection(all_columns - string_columns)
+
+
+def get_numeric_columns_pandas(df):
+    return df.select_dtypes(include="number")
+
+
+def get_branchwater_table(result, max_rows=None):
+    """Read in the branchwater search result csv"""
+    # These column names are accurate as of branchwater 0.9.5
+    # match_name is an SRA identifier when querying the SRA indexes
+    data_types = {
+        "query_name": "string",
+        "query_md5": "string",
+        "match_name": "string",
+        "containment": "float64",
+        "intersect_hashes": "int64",
+        "match_md5": "string",
+        "jaccard": "float64",
+        "max_containment": "float64",
+        "query_containment_ani": "float64",
+    }
+    return pd.read_csv(
+        result.file.path, index_col="match_name", nrows=max_rows, dtypes=data_types
+    )
 
 
 def apply_regex(rows, column, value):
@@ -100,51 +171,69 @@ def human_sort_key(text):
     ]
 
 
-def get_metadata(headers, rows):
-    # 0 = in the SRA mongodb database
-    # 1 = in the search result csv file
-    column_dict = {
-        "sra_link": 0,
-        "assay_type": 0,
-        "bioproject": 0,
-        "biosample_link": 0,
-        "query_containment_ani": 1,
-        "collection_date_sam": 0,
-        "containment": 1,
-        "geo_loc_name_country_calc": 0,
-        "lat_lon": 0,
-        "organism": 0,
-        "containment_threshold": 1,
-        "releasedate": 0,
-        "librarysource": 0,
-    }
-    mongo_df = search_mongodb(headers, rows)
-    csv_df = search_csv(headers, rows, column_dict)
-    combined_df = pd.DataFrame()
-    for column, in_csv in column_dict.items():
-        if in_csv == 1 and column in csv_df.columns:
-            combined_df[column] = csv_df[column]
-        elif in_csv == 0 and column in mongo_df.columns:
-            combined_df[column] = mongo_df[column]
-        else:
-            LOGGER.warning(
-                "Metadata column '%s' missing. Returning an empty string.", column
-            )
-            combined_df[column] = ""
-    new_headers = combined_df.columns.tolist()
-    new_rows = combined_df.values.tolist()
-    return new_headers, new_rows
+def prettify_column_names(df):
+    """Make column names look "nicer"
+
+    1. Replace underscores with spaces.
+    2. Convert to capital case
+
+    e.g. 'match_name' -> 'Match Name'
+
+    This won't universally give the perfect results but it should be good enough
+    """
+    df.columns = df.columns.replace("_", " ").capitalize()
+    return df
 
 
-def search_mongodb(headers, rows):
+def add_sra_metadata(branchwater_results):
+    branchwater_columns_for_output = [
+        "match_name",
+        "query_containment_ani",
+        "containment",
+        "containment_threshold",
+    ]
+    sra_columns = [
+        "sra_link",
+        "assay_type",
+        "bioproject",
+        "biosample_link",
+        "collection_date_sam",
+        "geo_loc_name_country_calc",
+        "lat_lon",
+        "organism",
+        "releasedate",
+        "librarysource",
+    ]
+    sra_accessions = branchwater_results["match_name"]
+    sra_metadata = search_mongodb(sra_accessions, sra_columns)
+    branchwater_subset_columns = branchwater_results[branchwater_columns_for_output]
+    branchwater_subset_columns.join(sra_metadata, validate="one_to_one")
+    return branchwater_subset_columns
+
+
+#    new_headers = branchwater_subset_columns.columns.tolist()
+#    new_rows = branchwater_subset_columns.values.tolist()
+#    return new_headers, new_rows
+
+
+def get_sra_fields(sra_accessions, fields):
     mongo = pm.MongoClient(settings.MONGO_URI)
     db = mongo["sradb"]
     collection = db["sradb_list"]
-    qidx = headers.index("match_name")
-    query = {"_id": {"$in": [row[qidx] for row in rows]}}
-    mongo_df = pd.DataFrame(list(collection.find(query)))
+    query = {"_id": {"$in": sra_accessions}}
+    results = list(collection.find(query))
     mongo.close()
-    return mongo_df.map(convert_to_string)
+    sra_metadata = pd.DataFrame(results)
+    found_columns = fields in sra_metadata.columns
+    if not all(found_columns):
+        LOGGER.warning(
+            "Not all SRA metadata fields were found in the SRA mongodb. Returning the subset of columns that were found."
+        )
+    sra_subset_columns = sra_metadata[found_columns]
+    # I'm not sure we need to convert_to_string here. Reenable this later if
+    # necessary
+    # return mongo_df.map(convert_to_string)
+    return sra_subset_columns
 
 
 def convert_to_string(value):
